ae7d2c512aaf85de198093e114ac03f4966596ba
Bori888 <ilyes.bori8@gmail.com>
2024-04-22 13:55:12 +0200

    session

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ else{
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="stilus.css">
 </head>
 <body>
     <?php
